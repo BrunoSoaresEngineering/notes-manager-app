c88c92fa512be6e58024f10f97f85efb865d0f18
st0rm348 <brunommsoares@outlook.com>
2024-08-05 14:45:41 +0100

    feat: add browser router logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,18 @@
+import { Routes, Route, Navigate } from 'react-router-dom';
+
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-    <h1>Hi</h1>
+    <Routes>
+      <Route path='/' element={<h1>Home</h1>}/>
+      <Route path='/new' element={<h1>New</h1>}/>
+      <Route path=':id'>
+        <Route index element={<h1>Show</h1>} />
+        <Route path='edit' element={<h1>Edit</h1>} />
+      </Route>
+      <Route path='*' element={<Navigate to='/'/>}/>
+    </Routes>
   )
 }
 
